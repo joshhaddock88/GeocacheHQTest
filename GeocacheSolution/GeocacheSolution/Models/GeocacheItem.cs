2b6af9180cb21b451dc52784d586e053aa431df0
Joshua Haddock <joshhaddock88@gmail.com>
2022-06-10 23:56:42 -0700

    Seeded data

--- GeocacheSolution/GeocacheSolution/Models/GeocacheItem.cs
+++ GeocacheSolution/GeocacheSolution/Models/GeocacheItem.cs
@@ -2,6 +2,7 @@
 {
     public class GeocacheItem
     {
+        public int ID { get; set; }
         public int GeocacheId { get; set; }
         public int ItemId { get; set; }
         public Item Item { get; set; }
