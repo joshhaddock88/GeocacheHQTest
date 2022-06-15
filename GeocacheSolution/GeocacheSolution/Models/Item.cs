ce1180280f54f4d12b86a2279c115648e09af629
Joshua Haddock <joshhaddock88@gmail.com>
2022-06-15 13:16:25 -0700

    Refactored ItemsController, added new validation to Models

--- GeocacheSolution/GeocacheSolution/Models/Item.cs
+++ GeocacheSolution/GeocacheSolution/Models/Item.cs
@@ -7,12 +7,13 @@ namespace GeocacheSolution.Models
     {
         public int ID { get; set; }
         [StringLength(50)]
-        [RegularExpression("^[A-Za-z 0-9]+$")]
+        [RegularExpression("^[A-Za-z 0-9]+$", ErrorMessage = "Name may only be numbers, letters, and spaces.")]
         [Required]
         public string Name { get; set; }
         public bool Active { get; set; }
         public int? GeocacheId { get; set; }
         public DateTime FirstActive { get; set; }
+        [Required(ErrorMessage = "Required Field.")]
         public DateTime LastActive { get; set; }
     }
 }
